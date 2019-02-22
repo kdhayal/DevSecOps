3503e8b9507d40a499175f2278a39c1f90a489cd
Kamalesh Dhayal <kamaleshdhayal2006@gmail.com>
2019-02-22 14:59:27 +0530

    Update ip-config.sh

--- scripts/ip-config.sh
+++ scripts/ip-config.sh
@@ -1,3 +1,3 @@
 TRAVIS_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' gruyere) 
 
-echo "default: TARGET_HOSTNAME=$TRAVIS_IP" > ./config/cucumber.yml
+echo "default: TARGET_HOSTNAME=$TRAVIS_IP" > ./cucumber.yml
